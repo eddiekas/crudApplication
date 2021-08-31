5c7b192fd198e260bf136556afd0b9aef41f9ed4
eddiekas <edstation4@gmail.com>
2021-08-31 17:14:14 +0300

    first commit

---
+++ connection.php
@@ -0,0 +1,89 @@
+<?php
+
+session_start();
+
+
+$mysqli = new mysqli('localhost', 'root', 'toors', 'crud') or die(mysqli_error($mysqli));
+
+$id = 0;
+$updatesave = false;
+$name = '';
+$role = '';
+
+
+
+
+if (isset($_POST['save']) && !empty($_POST['name']) && !empty($_POST['role'])) {
+  // code...
+  $name = $_POST['name'];
+  $role = $_POST['role'];
+
+
+  $mysqli->query("INSERT INTO employees (name, roles) VALUES('$name', '$role')")
+  or die($mysqli->error());
+
+  $_SESSION['message'] = "Record has been saved!";
+  $_SESSION['msg_type'] = "info";
+
+  header("location:index.php");
+
+}elseif (isset($_POST['save'])) {
+
+  $name = '';
+  $role = '';
+  $_SESSION['message'] = "Input Record!";
+  $_SESSION['msg_type'] = "danger";
+  header("location:index.php");
+
+}
+
+
+if (isset($_GET['delete'])) {
+  // code...
+  $id = $_GET['delete'];
+  $mysqli->query("DELETE FROM employees WHERE id=$id") or die($mysqli->error());
+
+  $_SESSION['message'] = "Record has been deleted!";
+  $_SESSION['msg_type'] = "danger";
+
+  header("location:index.php");
+
+}
+
+if (isset($_GET['update'])) {
+  // code...
+  $id = $_GET['update'];
+  $updatesave = true;
+  $result = $mysqli->query("SELECT * FROM employees WHERE id=$id") or die($mysqli->error());
+  if (!empty($result)) {
+    // code...
+    $row = $result->fetch_assoc();
+    $name = $row['name'];
+    $role = $row['roles'];
+
+
+
+  }
+
+}
+
+
+if (isset($_POST['finalUpdate'])) {
+  // code...
+
+  $id = $_POST['id'];
+  $name = $_POST['name'];
+  $role = $_POST['role'];
+
+  $mysqli->query("UPDATE employees SET name = '$name', roles = '$role' WHERE id = $id") or die($mysqli->error());
+
+  $_SESSION['message'] = "Record has been Updated";
+  $_SESSION['msg_type'] = "primary";
+
+  header('location:index.php');
+
+}
+
+
+
+ ?>
---
+++ index.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>CRUD!</title>
+  </head>
+  <body class="bg-info">
+
+    <?php require_once 'connection.php'; ?>
+
+    <?php
+
+    if (isset($_SESSION['message'])): ?>
+
+    <div class="alert alert-<?=$_SESSION['msg_type']?>">
+
+      <?php echo $_SESSION['message'];
+            unset($_SESSION['message']);
+       ?>
+
+    </div>
+  <?php endif; ?>
+
+
+    <div class="container">
+
+
+
+    <?php
+
+    $mysqli = new mysqli('localhost', 'root', 'toors', 'crud') or die(mysqli_error($mysqli));
+    $result = $mysqli->query("SELECT * FROM employees") or die($mysqli->error);
+
+    //pre_r($result);
+    // pre_r($result->fetch_assoc());
+
+    ?>
+
+    <div class="row justify-content-center">
+      <h1>cREATE, rEAD, uPDATE, dELETE</h1>
+
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Roles</th>
+            <th>Implement</th>
+          </tr>
+        </thead>
+
+        <?php
+
+        while ($row = $result->fetch_assoc()): ?>
+        <tr>
+          <td><?php echo $row['name']; ?></td>
+          <td><?php echo $row['roles']; ?></td>
+          <td><a href="index.php?update=<?php echo $row['id']; ?>"
+                  class="btn btn-success btn-sm">Update</a>
+              <a href="connection.php?delete=<?php echo $row['id']; ?>"
+                class="btn btn-danger btn-sm">Delete</a>
+
+          </td>
+        </tr>
+
+      <?php endwhile; ?>
+
+      </table>
+
+    </div>
+
+
+
+    <?php
+    function pre_r($array){
+
+      echo '<pre>';
+      print_r($array);
+      echo '</pre>';
+    }
+     ?>
+
+
+
+
+<div class="row justify-content-center">
+
+<form class="" action="connection.php" method="post">
+
+  <input type="hidden" name="id" value="<?php echo $id; ?>">
+
+<div class="form-row">
+
+  <div class="form-group col-md-6">
+  <label for="">Name: </label>
+  <input type="text" name="name" class="form-control" value="<?php echo $name; ?>" placeholder="Kindly Enter Name">
+  </div>
+
+  <div class="form-group col-md-6">
+  <label for="">Role: </label>
+  <input type="text" name="role" class="form-control" value="<?php echo $role; ?>"placeholder="Kindly Enter Role">
+  </div>
+</div>
+
+
+  <div class="form-group">
+
+    <?php if ($updatesave == true):  ?>
+      <button type="submit" class="btn btn-primary" name="finalUpdate">Update</button>
+    <?php else: ?>
+
+  <button type="submit" class="btn btn-info" name="save">Save</button>
+  <?php endif; ?>
+  </div>
+
+
+</form>
+</div>
+
+
+</div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
